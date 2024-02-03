c029a68d10c097726247ba79f58d0c053a94df72
OwenMeima04 <sirivanbiscuit@outlook.com>
2024-02-03 10:11:07 -0500

    dlib

--- scripts/face_recog_setup.py
+++ scripts/face_recog_setup.py
@@ -2,5 +2,23 @@ import cv2
 
 class RecogManager:
     
-    def __init__(self, path):
-        self.face_casc = cv2.CascadeClassifier
+    def __init__(self, path='xml/haarcascade_frontalface_default.xml'):
+        self.face_casc = cv2.CascadeClassifier(path)
+    
+    def recog(self, path):
+        img = cv2.imread(path)
+        grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        
+        face_find = self.face_casc.detectMultiScale(
+            grey, 
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30,30)
+            )
+        
+        return not len(face_find)
+
+
+if __name__=="__main__":
+    manager = RecogManager()
+    
