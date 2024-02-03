a44b7af970082cd9b72842d95a6bba0b0156c097
OwenMeima04 <sirivanbiscuit@outlook.com>
2024-02-03 10:38:58 -0500

    test

--- scripts/face_recog_setup.py
+++ scripts/face_recog_setup.py
@@ -1,24 +1,33 @@
 import cv2
+import dlib
+
+import face_recognition
+
 
 class RecogManager:
     
-    def __init__(self, path='xml/haarcascade_frontalface_default.xml'):
-        self.face_casc = cv2.CascadeClassifier(path)
+    def __init__(self):
+        self.known_faces = {}
+        self.add_known("Owen", "scripts/imgs/img1.jpg")
+        
+    def add_known(self, name, path):
+        image = face_recognition.load_image_file(path)
+        self.known_faces[name] = \
+            face_recognition.face_encodings(image)[0]
     
-    def recog(self, path):
-        img = cv2.imread(path)
-        grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        
-        face_find = self.face_casc.detectMultiScale(
-            grey, 
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30,30)
-            )
-        
-        return not len(face_find)
+    def recog_face(self, path):
+        u_image = face_recognition.load_image_file(path)
+        u_enc = face_recognition.face_encodings(u_image)
+        if not u_enc: return 'This is not a person'
+        for name, enc in self.known_faces.items():
+            matches = face_recognition.compare_faces([enc], u_enc[0])
+            if all(matches): return f'This is a picture of {name}'
+        return 'This is a person, but not Owen'
 
 
 if __name__=="__main__":
     manager = RecogManager()
+    test_path = "scripts/imgs/img4.jpg"
+    result = manager.recog_face(test_path)
+    print(result)
     
